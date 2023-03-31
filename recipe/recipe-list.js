5fd09ecdbb35e15c6380b9205a2d5002121e4a77
Robert Stephens <robert.stephens@accenturefederal.com>
2023-03-31 08:10:08 -0500

    some styling changes

--- recipe/recipe-list.js
+++ recipe/recipe-list.js
@@ -56,3 +56,5 @@ function displayRecipes() {
         <hr>`;
     });
 }
+
+displayRecipes();
--- recipe/script.js
+++ recipe/script.js
@@ -8,30 +8,43 @@ const currentList = document.querySelector('#target');
 // Define recipes array
 let recipes = [];
 
-let data = {
-  "response": {
-      "docs": [
-          {
-              "content_type": "article",
-              "description": "<p></p>",
-              "image_url": ["/img/baked-rigitoni.jpg"],
-              "title": "baked-rigitoni"
-          },
-          {
-              "content_type": "article",
-              "description": "<p></p>",
-              "image_url": ["/img/bolognese.jpg"],
-              "title": "bolognese"
-          },
-          {
-              "content_type": "article",
-              "description": "<p></p>",
-              "image_url": ["/img/fettuccine-alfredo.jpg"],
-              "title": "fettuccine-alfredo"
-          }
-      ]
-  }
-};
+// let data = {
+//   "response": {
+//       "docs": [
+//           {
+//               "content_type": "article",
+//               "description": ["<li>1 tablespoon butter</li>",
+//                               "<li>2 tablespoons olive oil</li>",
+//                               "<li>1 onion (chopped)</li>",
+//                               "<li>1 (26-ounce) jar tomato pasta sauce (or homemade )</li>",
+//                               "<li>3 tablespoons water</li>"],
+//               "image_url": ["/img/baked-rigitoni.jpg"],
+//               "title": "baked-rigitoni"
+//           },
+//           {
+//               "content_type": "article",
+//               "description": ["<li>1 tablespoon butter</li>",
+//                               "<li>2 tablespoons olive oil</li>",
+//                               "<li>1 onion (chopped)</li>",
+//                               "<li>1 (26-ounce) jar tomato pasta sauce (or homemade )</li>",
+//                               "<li>3 tablespoons water</li>"],
+//               "image_url": ["/img/bolognese.jpg"],
+//               "title": "bolognese"
+//           },
+//           {
+//               "content_type": "article",
+//               "description": ["<li>226 grams fettuccine (- ½ pound)</li>",
+//                               "<li>6 cups water</li>",
+//                               "<li>1 to 1.5 teaspoons salt (or add as required)</li>",
+//                               "<li>3 to 4 tablespoons Butter (- 30 grams, unsalted or salted)</li>",
+//                               "<li>½ teaspoon garlic (- finely chopped)</li>"],
+//               "image_url": ["/img/fettuccine-alfredo.jpg"],
+//               "title": "fettuccine-alfredo"
+//           }
+//       ]
+//   }
+// };
+
 
 // Handle form submit
 function handleSubmit(event) {
